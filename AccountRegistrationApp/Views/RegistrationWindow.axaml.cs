3fd34c4ef879381c99a1dd11116f0ae60dc67b6e
emodkid <ivan.galkin087@gmail.com>
2026-06-15 13:05:35 +0300

    add registration window

---
+++ AccountRegistrationApp/Views/RegistrationWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AccountRegistrationApp.Views;
+
+public partial class RegistrationWindow : Window
+{
+    public RegistrationWindow()
+    {
+        InitializeComponent();
+    }
+}
