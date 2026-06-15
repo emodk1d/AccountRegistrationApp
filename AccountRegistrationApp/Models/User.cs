07444d1690e5e04beaefda77b5dcfc546026a6e9
emodkid <ivan.galkin087@gmail.com>
2026-06-15 11:33:15 +0300

    rename

--- AccountRegistrationApp/Models/User.cs
+++ AccountRegistrationApp/Models/User.cs
@@ -6,7 +6,7 @@ public class User
 {
     public int Id { get; set; }
     public string Username { get; set; } = string.Empty;
-    public string PasswordHash { get; set; } = string.Empty; 
+    public string Password { get; set; } = string.Empty; 
     public bool IsAdmin { get; set; }
     public DateTime RegisteredAt { get; set; } = DateTime.Now;
 }
